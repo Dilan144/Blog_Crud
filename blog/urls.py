82ee0f60284638854285d255597ed807acf59409
Dilan144 <castrodilan.hd@gmail.com>
2025-10-04 16:43:34 -0400

    delete

--- blog/urls.py
+++ blog/urls.py
@@ -4,12 +4,13 @@ from .views import (
                     PostDetailView, 
                     PostCreateView, 
                     PostUpdateView,
+                    PostDeleteView
             )
                     
-
 urlpatterns = [
     path("", PostListView.as_view(), name="post-list"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/new/", PostCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/editar/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/eliminar/", PostDeleteView.as_view(), name="post-delete"),
 ]
