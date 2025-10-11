072c34a5dd7cbeffaa2fa2fb6da24b2f47ae01e2
Dilan144 <castrodilan.hd@gmail.com>
2025-10-11 16:00:34 -0400

    added sigup

--- django_base/urls.py
+++ django_base/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls")),
     path("accounts/", include("django.contrib.auth.urls")),  # New line for auth URLs
+    path("accounts/", include("accounts.urls")),  # New line for accounts app URLs
 ]
